a41fda1e2a9dfdeec2a32b96d6df10e678a0fbe4
LEE MINJI <m04j00@gmail.com>
2021-07-22 09:41:01 +0900

    Initial commit

---
+++ app/src/main/java/com/m0_0m/weatherdustchecker/WeatherMainActivity.kt
@@ -0,0 +1,11 @@
+package com.m0_0m.weatherdustchecker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WeatherMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.weather_main_activity)
+    }
+}
