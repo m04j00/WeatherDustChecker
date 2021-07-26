53f4d518bd44b94b1865ee459934f9613dd2af4f
LEE MINJI <m04j00@gmail.com>
2021-07-27 01:25:51 +0900

    날씨 프래그먼트 구현

--- app/src/main/java/com/m0_0m/weatherdustchecker/WeatherPageFragment.kt
+++ app/src/main/java/com/m0_0m/weatherdustchecker/WeatherPageFragment.kt
@@ -9,13 +9,27 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import java.net.URL
 
 class WeatherPageFragment :Fragment() {
 
+    @JsonIgnoreProperties(ignoreUnknown =  true)
+    data class OpenWeatherAPISONResponse(val main: Map<String, String>, val weather: List<Map<String, String>>)
+
+    val appID = ""
     lateinit var statusText : TextView
     lateinit var temperatureText : TextView
     lateinit var weatherImage : ImageView
+
+
     companion object {
         fun newInstance(lat : Double, lon : Double) : WeatherPageFragment{
             val fragment = WeatherPageFragment()
@@ -29,6 +43,25 @@ class WeatherPageFragment :Fragment() {
         }
     }
 
+    @JsonDeserialize(using=PersonDeserializer::class)
+    data class Person(var name: String, var age: Int, var address: Address)
+    data class Address(var city: String)
+
+    class PersonDeserializer : StdDeserializer<Person>(Person::class.java) {
+        override fun deserialize(parser: JsonParser?, ctxt: DeserializationContext?): Person {
+            Log.d("mytag", "from deserialize")
+            val node = parser?.codec?.readTree<JsonNode>(parser)
+
+            // 전달받은 JSON 객체를 탐색해서 필요한 데이터를 추출
+            val name = node?.get("name")?.asText()!!
+            val age = node?.get("age")?.asInt()!!
+            val address = node?.get("address")
+            val city = address?.get("city")?.asText()!!
+
+            return Person(name, age, Address(city))
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstState: Bundle?): View {
         val view = inflater.inflate(R.layout.weather_page_fragment, container, false)
 
@@ -39,7 +72,6 @@ class WeatherPageFragment :Fragment() {
         return view
     }
 
-    val appID = ""
 
     @SuppressLint("UseRequireInsteadOfGet")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -48,10 +80,60 @@ class WeatherPageFragment :Fragment() {
         val lon = arguments!!.getDouble("lng")
         val url = "http://api.openweathermap.org/data/2.5/weather?units=metric&appid=${appID}&lat=${lat}&lon=${lon}"
 
-        APICall(object : APICall.APICallback {
-            override fun onComplete(result: String) {
-                Log.d("myapp", result)
-            }
+
+/*        val data = mapper?.readValue<OpenWeatherAPIJSONResponse>(result)
+
+        val temp = data.main["temp"]
+
+
+        */
+
+            APICall(object : APICall.APICallback {
+
+                override fun onComplete(result: String) {
+                    Log.d("myapp", result)
+
+                    val mapper = jacksonObjectMapper()
+                    var data = mapper?.readValue<OpenWeatherAPISONResponse>(result)
+
+                    val temp = data.main.get("temp")
+
+                    temperatureText.text = temp
+                    val id = data.weather[0].get("id")
+                    if(id != null){
+                        statusText.text = when{
+                            id.startsWith("2") -> {
+                                weatherImage.setImageResource(R.drawable.flash)
+                                "천둥, 번개"
+                            }
+                            id.startsWith("3") -> {
+                                weatherImage.setImageResource(R.drawable.rain)
+                                "이슬비"
+                            }
+                            id.startsWith("5") -> {
+                                weatherImage.setImageResource(R.drawable.rain)
+                                "비"
+                            }
+                            id.startsWith("6") -> {
+                                weatherImage.setImageResource(R.drawable.snow)
+                                "눈"
+                            }
+                            id.startsWith("7") -> {
+                                weatherImage.setImageResource(R.drawable.cloudy)
+                                "흐림"
+                            }
+                            id.equals("800") -> {
+                                weatherImage.setImageResource(R.drawable.sun)
+                                "화창"
+                            }
+                            id.startsWith("8") -> {
+                                    weatherImage.setImageResource(R.drawable.cloud)
+                                    "구름 낌"
+                            }
+                            else -> "알 수 없음"
+                        }
+                    }
+                }
         }).execute(URL(url))
 
 
