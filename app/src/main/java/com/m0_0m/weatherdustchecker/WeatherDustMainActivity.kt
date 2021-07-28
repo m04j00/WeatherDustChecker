16abce557794c439c575f5bfa6b83003eb2a4d7e
LEE MINJI <m04j00@gmail.com>
2021-07-28 16:50:47 +0900

    위치 정보 접근 구현

--- app/src/main/java/com/m0_0m/weatherdustchecker/WeatherDustMainActivity.kt
+++ app/src/main/java/com/m0_0m/weatherdustchecker/WeatherDustMainActivity.kt
@@ -1,13 +1,18 @@
 package com.m0_0m.weatherdustchecker
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
@@ -16,8 +21,8 @@ import java.lang.Exception
 
 class WeatherDustMainActivity : AppCompatActivity() {
     private lateinit var pager : ViewPager
-    private var lat: Double = 37.4665708044902
-    private var lon : Double = 126.93287991349258
+    private var lat: Double = 37.39287945527131
+    private var lon : Double = 129.22019748428113
 
     private lateinit var locationManager : LocationManager
     private lateinit var locationListener: LocationListener
@@ -34,6 +39,9 @@ class WeatherDustMainActivity : AppCompatActivity() {
             override fun onLocationChanged(location: Location) {
                 lat = location.latitude
                 lon = location.longitude
+
+                locationManager.removeUpdates(this)
+
             }
         }
         pager = findViewById<ViewPager>(R.id.pager)
@@ -51,7 +59,7 @@ class WeatherDustMainActivity : AppCompatActivity() {
 
             override fun onPageSelected(position: Int) {
                 if(position == 0){
-                    Toast.makeText(this@WeatherDustMainActivity,"날시 페이지입니다.", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@WeatherDustMainActivity,"날씨 페이지입니다.", Toast.LENGTH_SHORT).show()
                 }else if(position == 1){
                     Toast.makeText(this@WeatherDustMainActivity,"미세먼지 페이지입니다.", Toast.LENGTH_SHORT).show()
                 }
@@ -60,7 +68,51 @@ class WeatherDustMainActivity : AppCompatActivity() {
             override fun onPageScrollStateChanged(state: Int) {        }
 
         })
+
+        if(
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED
+                &&
+                ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED
+        ){
+            locationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    0,
+                    0f,
+                    locationListener
+            )
+        } else{
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSION_REQUEST_CODE)
+        }
     }
+
+    @SuppressLint("MissingSuperCall", "MissingPermission")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if(requestCode == PERMISSION_REQUEST_CODE){
+            var allPermissionGranted = true
+            for(result in grantResults){
+                allPermissionGranted = (result == PackageManager.PERMISSION_GRANTED)
+                if(!allPermissionGranted) break
+            }
+            if(allPermissionGranted){
+                locationManager.requestLocationUpdates(
+                        LocationManager.NETWORK_PROVIDER,
+                        0,
+                        0f,
+                        locationListener
+                )
+            } else{
+                Toast.makeText(this, "위치 정보 제공 동의가 필요합니다.", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+            Log.d("mytag", "${lat}, ${lon}")
+        }
+    }
+
     inner class MyPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm){
         override fun getCount() = 2
 
